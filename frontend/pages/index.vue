ed89759805cc2d8c63c5b75343af3c947bb252f5
Remi <prialgauskis.remigijus@gmail.com>
2024-02-15 14:15:20 +0200

    mazi ui pakeitimai

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -10,22 +10,37 @@
         />
         <div class="absolute inset-0 flex items-center justify-center flex-col">
           <div class="text-center mb-4">
-            <h1 class="text-white text-6xl font-semibold mb-2">
-              <span class="bounce">H</span>
-              <span class="bounce" style="animation-delay: 0.2s">e</span>
-              <span class="bounce" style="animation-delay: 0.4s">l</span>
-              <span class="bounce" style="animation-delay: 0.6s">l</span>
+            <h1 class="text-white text-5xl font-semibold mb-6">
+              <span class="bounce">c</span>
+              <span class="bounce" style="animation-delay: 0.2s">o</span>
+              <span class="bounce" style="animation-delay: 0.4s">n</span>
+              <span class="bounce" style="animation-delay: 0.6s">s</span>
               <span class="bounce" style="animation-delay: 0.8s">o</span>
-              <span class="bounce" style="animation-delay: 1s">,</span>
-              <span class="bounce" style="animation-delay: 1.2s"> </span>
-              <span class="bounce" style="animation-delay: 1.4s">W</span>
-              <span class="bounce" style="animation-delay: 1.6s">o</span>
-              <span class="bounce" style="animation-delay: 1.8s">r</span>
-              <span class="bounce" style="animation-delay: 2s">l</span>
-              <span class="bounce" style="animation-delay: 2.2s">d</span>
-              <span class="bounce" style="animation-delay: 2.4s">!</span>
+              <span class="bounce" style="animation-delay: 1s">l</span>
+              <span class="bounce" style="animation-delay: 1.2s">e</span>
+              <span class="bounce" style="animation-delay: 1.4s">.</span>
+              <span class="bounce" style="animation-delay: 1.6s">l</span>
+              <span class="bounce" style="animation-delay: 1.8s">o</span>
+              <span class="bounce" style="animation-delay: 2s">g</span>
+              <span class="bounce" style="animation-delay: 2.2s">(</span>
+              <span class="bounce" style="animation-delay: 2.4s">"</span>
+              <span class="bounce" style="animation-delay: 2.6s">H</span>
+              <span class="bounce" style="animation-delay: 2.8s">e</span>
+              <span class="bounce" style="animation-delay: 3s">l</span>
+              <span class="bounce" style="animation-delay: 3.2s">l</span>
+              <span class="bounce" style="animation-delay: 3.4s">o</span>
+              <span class="bounce" style="animation-delay: 3.6s">,</span>
+              <span class="bounce" style="animation-delay: 3.8s"> </span>
+              <span class="bounce" style="animation-delay: 4s">W</span>
+              <span class="bounce" style="animation-delay: 4.2s">o</span>
+              <span class="bounce" style="animation-delay: 4.4s">r</span>
+              <span class="bounce" style="animation-delay: 4.6s">l</span>
+              <span class="bounce" style="animation-delay: 4.8s">d</span>
+              <span class="bounce" style="animation-delay: 5s">!</span>
+              <span class="bounce" style="animation-delay: 5.2s">"</span>
+              <span class="bounce" style="animation-delay: 5.4s">)</span>
             </h1>
-            <p class="text-white text-lg">Pasiruošę išbandyti savo jėgas?</p>
+            <p class="text-white text-xl">Pasiruošę išbandyti savo jėgas?</p>
           </div>
 
           <div class="absolute bottom-0 scale-x-150 w-full flex justify-center">
@@ -137,7 +152,7 @@
   animation-name: bounce;
   animation-duration: 0.5s; /* speed of each bounce */
   animation-fill-mode: both; /* keep the view in the end state of the last keyframe when the animation is not running */
-  animation-iteration-count: infinite; /* makes the animation repeat infinitely */
+  animation-iteration-count: 1; /* makes the animation repeat infinitely */
 }
 
 /* Set the animation delay based on the index of the letter */
@@ -178,7 +193,51 @@
 .bounce:nth-child(12) {
   animation-delay: 5.5s;
 }
-
+.bounce:nth-child(13) {
+  animation-delay: 0s;
+}
+.bounce:nth-child(14) {
+  animation-delay: 0.5s;
+}
+.bounce:nth-child(15) {
+  animation-delay: 1s;
+}
+.bounce:nth-child(16) {
+  animation-delay: 1.5s;
+}
+.bounce:nth-child(17) {
+  animation-delay: 2s;
+}
+.bounce:nth-child(18) {
+  animation-delay: 2.5s;
+}
+.bounce:nth-child(19) {
+  animation-delay: 3s;
+}
+.bounce:nth-child(20) {
+  animation-delay: 3.5s;
+}
+.bounce:nth-child(21) {
+  animation-delay: 4s;
+}
+.bounce:nth-child(22) {
+  animation-delay: 4.5s;
+}
+.bounce:nth-child(23) {
+  animation-delay: 5s;
+}
+.bounce:nth-child(24) {
+  animation-delay: 5.5s;
+}
+.bounce:nth-child(25) {
+  animation-delay: 4.5s;
+}
+.bounce:nth-child(26) {
+  animation-delay: 5s;
+}
+.bounce:nth-child(27) {
+  animation-delay: 5.5s;
+}
 /* Set an animation interval */
 /* Total time for one full cycle of animation */
 /* This should be the sum of the individual animation duration (0.5s) and delays */
